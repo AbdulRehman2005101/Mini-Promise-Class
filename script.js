973780caa17c60be44925dc51de4567c4e616b63
AbdulRehman <abdulrehmanamjad2005101@gmail.com>
2025-06-23 22:44:28 +0500

    my own proise class added

---
+++ script.js
@@ -0,0 +1,31 @@
+class fakePromise{
+    constructor(executer){
+        this.onSuccess=null;
+        this.onfailure-null;
+
+        this.resolve=(value)=>{
+            if(this.onSuccess){
+                this.onSuccess(value);
+            }
+        }
+
+        this.reject=(value)=>{
+            if(this.onfailure){
+                this.onfailure(value);
+            }
+        }
+
+        executer(this.resolve,this.reject);
+    }
+
+    then(func){
+        this.onSuccess=func;
+        return this;
+    }
+
+    catch(func){
+        this.onfailure=func;
+        return this;
+    }
+}
+
